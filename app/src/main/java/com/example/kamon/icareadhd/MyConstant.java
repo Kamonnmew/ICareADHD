e28ac09c1ea408e07b41ac7c45f51a70fa7b9814
masterUNG <phrombutr@gmail.com>
2017-02-12 10:21:48 +0700

    Near Finish

---
+++ app/src/main/java/com/example/kamon/icareadhd/MyConstant.java
@@ -0,0 +1,16 @@
+package com.example.kamon.icareadhd;
+
+/**
+ * Created by masterUNG on 2/12/2017 AD.
+ */
+
+public class MyConstant {
+
+    //Explicit
+    private String[] loginButtonMain = new String[]{"Login", "เข้าสู่ระบบ"};
+
+
+    public String[] getLoginButtonMain() {
+        return loginButtonMain;
+    }
+}   // Main Class
